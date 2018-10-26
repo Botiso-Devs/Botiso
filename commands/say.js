6b9b2da06929a8dcb21aaefcdb447fa3d70bd444
Botiso-Devs <44485369+Botiso-Devs@users.noreply.github.com>
2018-10-26 09:53:46 -0400

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+if
+(message.content.indexOf(process.env.PREFIX) !== 0) return;
+
+      if(!message.member.hasPermission("MANAGE_MESSAGES")) return;
+      const sayMessage = args.join(" ");
+      message.delete().catch();
+      message.channel.send(sayMessage);
+
+}
+
+module.exports.help = {
+  name: "say"
+}
