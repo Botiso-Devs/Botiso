9ec4b1897560bcd63623bf22dcf31217ba36f905
Botiso-Devs <44485369+Botiso-Devs@users.noreply.github.com>
2018-10-26 06:29:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,4 +18,23 @@ bot.on("ready", async () => {
  bot.user.setActivity("Coding Central", {type: "PLAYING"});
  });
 
+bot.on("message", (message) => {
+ if(message.content === ".uptime") {
+  let totalSeconds = (bot.uptime / 1000);
+let hours = Math.floor(totalSeconds / 3600);
+totalSeconds %= 3600;
+let minutes = Math.floor(totalSeconds / 60);
+let seconds = Math.floor(totalSeconds % 60);
+let uptime = `*${hours} hours, ${minutes} minutes and ${seconds} seconds*`;
+  
+  let uptimee = new Discord.RichEmbed()
+  .setColor("RED")
+  .setTitle("Botiso™ Uptime Command")
+  .addField(`󠂪 󠂪`, `**Uptime**: ${uptime}\n󠂪 󠂪`, true)
+  .setFooter(`Requested By ${message.author.username}`, `${message.author.avatarURL}`)
+  .setTimestamp()
+ 
+  message.channel.aend(uptimee)
+});
+ 
 bot.login(process.env.BOT_TOKEN);
