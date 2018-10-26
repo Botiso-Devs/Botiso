b2cbfdd875badd11c70116dbed992993f78d9497
Botiso-Devs <44485369+Botiso-Devs@users.noreply.github.com>
2018-10-25 22:05:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const botconfig = require("./botconfig.json");
+
 const Discord = require("discord.js");
 const fs = require("fs");
 
@@ -30,7 +30,7 @@ bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
-  let prefix = botconfig.prefix;
+  let prefix = process.env.PREFIX;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
