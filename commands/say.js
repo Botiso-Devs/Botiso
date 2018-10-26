66965d840d1a6a96ae55f0a2774c77617b759c23
Botiso-Devs <44485369+Botiso-Devs@users.noreply.github.com>
2018-10-26 09:55:13 -0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 if
 (message.content.indexOf(process.env.PREFIX) !== 0) return;
 
-      if(!message.member.hasPermission("MANAGE_MESSAGES")) return;
+      if(!message.member.hasPermission("SEND_MESSAGES")) return;
       const sayMessage = args.join(" ");
       message.delete().catch();
       message.channel.send(sayMessage);
