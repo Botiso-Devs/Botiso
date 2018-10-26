abbe023f1a1ef0b487808fbfdf1bff2365eac0c8
Botiso-Devs <44485369+Botiso-Devs@users.noreply.github.com>
2018-10-26 09:49:45 -0400

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
   if
     (message.content.indexOf(process.env.PREFIX) !== 0) return;
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("U Need Manage Message To Work This Command!");
+  if(!message.member.hasPermission("SEND_MESSAGES")) return message.reply("U Need Manage Message To Work This Command!");
   if(!args[0]) return message.channel.send("U Need Manage Message To Work This Command!");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
