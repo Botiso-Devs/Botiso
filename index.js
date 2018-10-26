8a3520c881e7cd159d2be661b47a83cc8ff4cd0e
Botiso-Devs <44485369+Botiso-Devs@users.noreply.github.com>
2018-10-26 06:06:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,21 +9,7 @@ bot.on("message", (message) => {
 
 bot.on("message", (message) => {
  if(message.content === ".uptime") {
-let totalSeconds = (bot.uptime / 1000);
-let hours = Math.floor(totalSeconds / 3600);
-totalSeconds %= 3600;
-let minutes = Math.floor(totalSeconds / 60);
-let seconds = Math.floor(totalSeconds % 60);
-let uptime = `*${hours} hours, ${minutes} minutes and ${seconds} seconds*`;
-  
-  let uptimee = new Discord.RichEmbed()
-  .setColor("RED")
-  .setTitle("Botiso™ Uptime Command")
-  .addField(`󠂪 󠂪`, `**Uptime**: ${uptime}\n󠂪 󠂪`, true)
-  .setFooter(`Requested By ${message.author.username}`, `${message.author.avatarURL}`)
-  .setTimestamp()
-  
-  message.channel.send(uptimee)
+  message.channel.send("Under Work")
   }
 });
 
