7f85a47f45c84fcec7e2913818f790f2ca8110e5
Botiso-Devs <44485369+Botiso-Devs@users.noreply.github.com>
2018-10-25 21:37:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const Discord = require("discord.js")
 const bot = new Discord.Client();
 
+bot.on("ready", () => {
+  console.log("Bot Online")
+
+bot.user.setActivity("24/7 BABY", {type: "PLAYING"})
+});
+
+
+
 bot.login(process.env.BOT_TOKEN);
