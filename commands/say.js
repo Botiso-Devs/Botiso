8dc6076efb8eafd5b9ef427fb176d48428049fd6
Botiso-Devs <44485369+Botiso-Devs@users.noreply.github.com>
2018-10-26 14:43:25 -0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,8 +3,6 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 if
 (message.content.indexOf(process.env.PREFIX) !== 0) return;
-if
- (message.hasPermission("MANAGE_MESSAGES")) return message.channel.send("```Sorry, You Do Not Have Sufficient Permission s To This Command```")
 
       if(!message.member.hasPermission("SEND_MESSAGES")) return;
       const sayMessage = args.join(" ");
