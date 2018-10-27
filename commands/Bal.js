05e29a03f014472cae697978f82483b9df325b81
Botiso-Devs <44485369+Botiso-Devs@users.noreply.github.com>
2018-10-26 22:23:33 -0400

    Update Bal.js

--- commands/Bal.js
+++ commands/Bal.js
@@ -3,7 +3,7 @@ module.exports.run = async (bot, message, args) => {
 if (message.content.indexOf(process.env.PREFIX) !== 0) return; 
 
 if (!userData[sender.id + message.guild.id]) userData[sender.id + message.guild.id] - {}
-if (!userData[sender.id + message.guild.id] userData[sender.id + message.guild.id].money = 1000;
+if (!userData[sender.id + message.guild.id].money) userData[sender.id + message.guild.id].money = 1000;
 
 fs.writeFile("Storage/userData.json", JSON.stringify(userData), (err) -> {
   if (err) console.log(err);
