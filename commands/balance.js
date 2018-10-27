8c0888e320d58f30f3281791547dbe4c2adf0992
Botiso-Devs <44485369+Botiso-Devs@users.noreply.github.com>
2018-10-27 00:19:27 -0400

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -2,14 +2,14 @@ const fs = require("fs")
 module.exports.run = async (bot, message, args) => {
 if (message.content.indexOf(process.env.PREFIX) !== 0) return; 
 
-let UserData = JSON.parse(fs.readFileSync('Storage/userData.json', "utf8"));
+let UserData = JSON.parse(fs.readFileSync('Storage/UserData.json', "utf8"));
 
 let sender = message.author
 
 if(!userData[sender.id + message.guild.id]) userData[sender.id + message.guild.id] = {}
 if(!userData[sender.id + mesdage.guild.id].money) userData[sender.id + message.guild.id].money = 1000;
 
-fs.writeFile('Storage/userData.json', JSON.stringify(userData), (err) => {
+fs.writeFile('Storage/UserData.json', JSON.stringify(userData), (err) => {
  if (err) console.log(err);
  })
  
