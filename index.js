dda426c7a7ce53fbf7d6fef2ce9d250993509073
Botiso-Devs <44485369+Botiso-Devs@users.noreply.github.com>
2018-10-28 21:41:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ fs.readdir("./commands/", (err, files) => {
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity(`Coding Central`, {type: "STREAMING"});
+  bot.user.setActivity(`MythicalPE Network`, {type: "STREAMING"});
 
 });
 
